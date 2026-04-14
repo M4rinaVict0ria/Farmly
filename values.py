083e8bb2d64a86bba635d1409b19a7da3a1b8860
Marina Freitas <160733141+M4rinaVict0ria@users.noreply.github.com>
2026-04-14 03:06:19 +0000

    coloquei coisa no values

---
+++ values.py
@@ -0,0 +1,11 @@
+vehicles = {
+    "carro": {
+        "nome": "Carro",
+        "obtainable": "Shop",
+        "nota": "Boa",
+        "valor": "10k",
+        "stability": "Alta",
+        "demand": "Alta",
+        "rarity": "Comum"
+    }
+}
